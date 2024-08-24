7fec479e139baee3e67e8256acc7af3b781c93b4
Daniel Neres <danielnerres@gmail.com>
2024-08-24 17:50:40 -0300

    atualização da movimentação

--- aulas/aula_2_alternativa.js
+++ aulas/aula_2_alternativa.js
@@ -14,8 +14,8 @@ renderer.setClearColor(0xDBDCDF);
 let theta = 0;
 let r = 4
 
-let cubes = [];
-let thetas = []; 
+let cubes_1 = [];
+let thetas_1 = []; 
 
 for (let i = 0; i < 10; i++){
 
@@ -27,23 +27,31 @@ for (let i = 0; i < 10; i++){
 	cube.position.x = r*Math.cos(-theta);
 	cube.position.z = r*Math.sin(-theta);
 	scene.add( cube );
-	cubes.push(cube);
-	thetas.push(theta);
+	cubes_1.push(cube);
+	thetas_1.push(theta);
 }
-/*
+
+theta = 0;
+let cubes_2 = [];
+let thetas_2 = []; 
+
 for (let i = 0; i < 10; i++){
 
 	theta += 0.65;
 	const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 	const material = new THREE.MeshBasicMaterial( { color: 0x006B5C, wireframe: false } );
 	const cube = new THREE.Mesh( geometry, material );
-	//cube.position.y = r*Math.cos(-theta);
-	cube.position.x = r*Math.sin(-theta);
-	//cube.position.x = r*Math.sin(-theta);
-	cubes.push(cube);
-	thetas.push(theta);
+	cube.position.y = r*Math.sin(theta);
+	cube.position.x = r*Math.cos(theta);
+	cube.position.z = r*Math.cos(theta);
+	cubes_2.push(cube);
+	thetas_2.push(theta);
     scene.add( cube );
-}*/
+}
+
+theta = 0;
+let cubes_3 = [];
+let thetas_3 = []; 
 
 
 const geometry_circulo = new THREE.SphereGeometry( 5);
@@ -58,20 +66,30 @@ controls.update();
 
 
 camera.position.z = 15;
+
 theta = 0;
 function animate() {
-
 	theta += 0.01;
-	console.log("oi");
-	for (var i in cubes){
-		let cube_var = cubes[i];
-		cube_var.position.y = r*Math.sin(thetas[i] + theta);
-		cube_var.position.x = r*Math.cos(thetas[i] + theta);
-		cube_var.position.z = r*Math.sin(thetas[i] + theta);
+	for (var i in cubes_1){
+		let cube_var = cubes_1[i];
+		cube_var.position.y = r*Math.sin(thetas_1[i] + theta);
+		cube_var.position.x = r*Math.cos(thetas_1[i] + theta);
+		cube_var.position.z = r*Math.sin(thetas_1[i] + theta);
 
 		cube_var.rotation.x += 0.01;
 		cube_var.rotation.y += 0.01;
 	}
+
+	for (var i in cubes_2){
+		let cube_var = cubes_2[i];
+		cube_var.position.y = r*Math.sin(thetas_2[i] - theta);
+		cube_var.position.x = r*Math.cos(thetas_2[i] - theta);
+		cube_var.position.z = r*Math.cos(thetas_2[i] - theta);
+
+		cube_var.rotation.x += 0.01;
+		cube_var.rotation.y += 0.01;
+	}
+
 
 	renderer.render( scene, camera );
 
