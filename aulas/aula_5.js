dc185fa60585ec013faff9753d296d2f2c418493
Daniel Neres <danielnerres@gmail.com>
2024-09-22 11:51:09 -0300

    aula_5

--- aulas/aula_5.js
+++ aulas/aula_5.js
@@ -9,10 +9,15 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const geometry = new THREE.BoxGeometry( 1, 1, 1, 5, 0, 5 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+
+/*const geometry_cilider = new THREE.
+const material_cilider= new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
+const cilider = new THREE.Mesh( geometry, material );
+scene.add( cilider );*/
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 0, 0);
@@ -25,6 +30,7 @@ function animate() {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+	
 
 	renderer.render( scene, camera );
 
